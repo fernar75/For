ebcb8c6fec4ffaddb321df7b89340f870b786025
fernar75 <94242277+fernar75@users.noreply.github.com>
2022-03-22 15:43:42 -0300

    Initial commit

---
+++ build/classes/pkgfor/For.rs
@@ -0,0 +1,1 @@
+pkgforfor.For
---
+++ src/pkgfor/For.java
@@ -0,0 +1,16 @@
+
+package pkgfor;
+
+
+public class For {
+
+        public static void main(String[] args) {
+        for (int cont = 0; cont <= 10; cont++) {
+            
+            System.out.println("Estoy en la vuelta " + cont);
+            
+        }
+        
+    }
+    
+}
